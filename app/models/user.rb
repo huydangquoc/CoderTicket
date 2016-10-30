a5f81185864d6d25983cb87bb75d671a1283add2
Dang Quoc Huy <huysamdua@yahoo.com>
2016-10-30 16:26:32 +0700

    support user feature

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_insensitive: false}
+  has_secure_password
+
+  def self.all_except user
+    where.not(id: user)
+  end
+end
