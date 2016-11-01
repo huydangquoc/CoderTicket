5ff43a2425ce93456760d3d351c209ee14be00a1
Dang Quoc Huy <huysamdua@yahoo.com>
2016-11-01 09:51:25 +0700

    Event without any ticket type can't be published

--- app/controllers/ticket_types_controller.rb
+++ app/controllers/ticket_types_controller.rb
@@ -1,10 +1,20 @@
 class TicketTypesController < ApplicationController
   before_filter :authenticate_user
 
+  def new
+    @event = Event.find params[:event_id]
+    @ticket_type = @event.ticket_types.new
+    session[:return_to] ||= request.referer
+  end
+
   def create
     @event = Event.find params[:event_id]
     @event.ticket_types.create(ticket_type_params)
-    redirect_to event_path(@event)
+    if session[:return_to]
+      redirect_to session.delete(:return_to)
+    else
+      redirect_to event_path(@event)
+    end
   end
 
   private
